81bc2e5d4cece1dcf594a2c119bb7dd3dc7c405a
Rishika-C <34199898+Rishika-C@users.noreply.github.com>
2026-05-04 19:04:37 +1200

    Fig 1 to aid with score test statistic explanation :)

--- RCode.R
+++ RCode.R
@@ -79,7 +79,7 @@ if (F) {
 # H1: mu_i=0 + beta_1
 
 # Function to create y-values for plot
-ll.diff.func = function(n, beta1.seq=seq(-1, 2, by=0.01), mean.y=1, which.beta.subtract=1) {
+ll.diff.func = function(n, beta1.seq=seq(-1.5, 2, by=0.01), mean.y=1, which.beta.subtract=1) {
   sum.y.sq = (1 + (mean.y^2)) * n # Based on Var(Y)=1/so that Var(Y)=1 for one observation still holds :)
   ll = (((n/2) * log(2*pi)) + (1/2)*sum.y.sq - (beta1.seq)*(n)*(mean.y) + (n/2)*(beta1.seq^2)) * (-1) # Have manually confirmed this expression is correct :)
   ll.subtract = ll[beta1.seq==which.beta.subtract] # Shifting curves accordingly -- so that for scenario 1, curves intersect at peak; for scenario 2, curves intersect at theta1-tilde
@@ -87,14 +87,35 @@ ll.diff.func = function(n, beta1.seq=seq(-1, 2, by=0.01), mean.y=1, which.beta.s
   ll.diff
 }
 
+beta1.seq = seq(-1.5, 2, by=0.01) # x seq for both plots
+
 # Scenario (1): different gradient at theta1-tilde, due to n only (not due to Var(Y) -- equivalently, not due to Y-bar or sum(Y_i^2). Shows how increasing sample size affects gradient, and therefore affects score statistic (as here, score = U^2/I)
-beta1.seq = seq(-1, 2, by=0.01)
-plot(beta1.seq, ll.diff.func(n=5), type="l", xlab=expression(beta[1]), ylab="log-likelihood difference")
+cairo_pdf("Figures/Fig1a.pdf")
+par(mgp=c(2.7, 1, 0))
+plot(beta1.seq, ll.diff.func(n=5), type="l", xlab="", ylab=expression("\u2113"(beta[1]) - "\u2113"(hat(beta)[1])), lwd=2, cex.lab=1.3, xaxt="n", cex.axis=1.2)
+axis(side=1, at=c(-1, -0.5, 0, 0.5, 1, 1.5, 2), labels=c("-1", "-0.5", expression(tilde(beta)[1]), "0.5", expression(hat(beta)[1]), "1.5", "2"), cex.axis=1.2, padj=0.3)
+title(xlab=expression(beta[1]), mgp=c(3, 1, 0), cex.lab=1.4)
 grid()
-lines(beta1.seq, ll.diff.func(n=10), col="blue")
+lines(beta1.seq, ll.diff.func(n=10), col="blue", lwd=2)
+abline(v=1, col="green", lty=2)
+legend("topleft", legend=c(expression(n~"="~5~", "~S~"="~"25/5"~"="~5),
+                           expression(n~"="~10~", "~S~"="~"100/10"~"="~10)), lwd=2, col=c("black", "blue"))
+dev.off()
+
+
 
 # Scenario (2): same score at theta1-tilde --> gradient alone is not enough! :)
-plot(beta1.seq, ll.diff.func(n=10, mean.y=0.5, which.beta.subtract=0), type="l", xlab=expression(beta[1]), ylab="log-likelihood difference", col='red', ylim=c(-10, 3))
-lines(beta1.seq, ll.diff.func(n=5, which.beta.subtract=0))
+ cairo_pdf("Figures/Fig1b.pdf")
+par(mgp=c(2.7, 1, 0))
+plot(beta1.seq, ll.diff.func(n=10, mean.y=0.5, which.beta.subtract=0), type="l", xlab="", ylab=expression("\u2113"(beta[1]) - "\u2113"(hat(beta)[1])), col='red', ylim=c(-10, 3), lwd=2, cex.lab=1.3, xaxt="n", cex.axis=1.2)
+axis(side=1, at=c(-1, -0.5, 0, 0.5, 1, 1.5, 2), labels=c("-1", "-0.5", expression(tilde(beta)[1]), "0.5", expression(hat(beta)[1]), "1.5", "2"), cex.axis=1.2, padj=0.3)
+title(xlab=expression(beta[1]), mgp=c(3, 1, 0), cex.lab=1.4)
+grid()
+lines(beta1.seq, ll.diff.func(n=5, which.beta.subtract=0),lwd=2)
+abline(v=0, col="green", lty=2)
+legend("topleft", legend=c(expression(U(tilde(beta)[1])~"="~5~", "~S~"="~5),
+                           expression(U(tilde(beta)[1])~"="~5~", "~S~"="~2.5)), lwd=2, col=c("black", "red"),
+       bg="white")
+dev.off()
 
 ###########################################################################################
